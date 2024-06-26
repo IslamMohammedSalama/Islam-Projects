3df6056500fea64f01899311c06679a0ed746bf8
IslamMohamedSalama <mstkhdmjwjl17@gmail.com>
2024-06-26 11:54:44 +0300

    y

--- Python/gui-apps/Shooter/shooter_tut13.py
+++ Python/gui-apps/Shooter/shooter_tut13.py
@@ -209,7 +209,7 @@ class Soldier(pygame.sprite.Sprite):
 
 		#jump
 		if self.jump == True: #  and self.in_air == False:
-			self.vel_y = -15
+			self.vel_y = -20
 			self.jump = False
 			self.in_air = True
 
