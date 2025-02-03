cf66d1ac3d0be24384f04c2940ddcd49d42098bf
IslamMohamedSalama <mstkhdmjwjl17@gmail.com>
2025-02-03 12:11:07 +0200

    Add JavaScript assignment solutions and create index.html for assignment 7

--- Web/JavaScript/script.js
+++ Web/JavaScript/script.js
@@ -105,3 +105,50 @@ console.log(
 ); // eLZERO WEB SCHOOl
  */
 
+/*
+  If Condition Challenge
+*/
+
+let a = 10;
+
+if (a < 10) {
+	console.log(10);
+} else if (a >= 10 && a <= 40) {
+	console.log("10 To 40");
+} else if (a > 40) {
+	console.log("> 40");
+} else {
+	console.log("Unknown");
+}
+
+a < 10
+	? console.log(10)
+	: a >= 10 && a <= 40
+	? console.log("10 To 40")
+	: a > 40
+	? console.log("> 40")
+	: console.log("Unknown");
+
+// Write Previous Condition With Ternary If Syntax
+
+let st = "Elzero Web School";
+
+if ((st.length * 2).toString() === "34") {
+	console.log("Good 1");
+}
+
+// W Position May Change
+if (st.charAt(st.toLowerCase().indexOf("w", 0)).toLowerCase() === "w") {
+	console.log("Good 2");
+}
+if (!typeof st.toString() !== "string") {
+	console.log("Good 3");
+}
+
+if (typeof st.length === "number") {
+	console.log("Good 4");
+}
+
+if (st.slice(0, 6).repeat(2) === "ElzeroElzero") {
+	console.log("Good 5");
+}
