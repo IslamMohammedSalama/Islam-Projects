563e3f83df5e285726e3caf1fea6f89237c03461
IslamMohamedSalama <mstkhdmjwjl17@gmail.com>
2025-03-18 12:57:33 +0200

    Refactor ripple effect implementation for button interactions

--- Web/Totorials/rappile-button/script.js
+++ Web/Totorials/rappile-button/script.js
@@ -1,15 +1,12 @@
-const delay = (ms) => new Promise((res) => setTimeout(res, ms));
-
 let button = document.querySelector("button");
-// button.onclick = function (e) {};
+
 window.addEventListener("mousedown", (e) => {
 	const target = e.target;
-
 	if (target.nodeName == "BUTTON") {
 		show_ripple(target);
-		// console.log("Yes")
 	}
 });
+
 function show_ripple(button) {
 	const style = getComputedStyle(button);
 	let ripple_elmnt = document.createElement("span");
@@ -21,49 +18,53 @@ function show_ripple(button) {
 	ripple_elmnt.style.position = "absolute";
 	ripple_elmnt.style.borderRadius = "50%";
 	ripple_elmnt.style.pointerEvents = "none";
-
 	ripple_elmnt.style.left = event.clientX - button.offsetLeft - radius + "px";
 	ripple_elmnt.style.top = event.clientY - button.offsetTop - radius + "px";
-
 	ripple_elmnt.style.transform = "scale(0)";
 	ripple_elmnt.style.transition = "transform 1s ease, opacity 0.5s ease";
 	ripple_elmnt.style.background = "rgba(255,255,255,0.5)";
 	button.appendChild(ripple_elmnt);
 
-	setTimeout(async () => {
+	setTimeout(() => {
 		ripple_elmnt.style.transform = "scale(5)";
-		ripple_elmnt.style.opacity = 0;
-		await delay(500);
-		button.removeChild(ripple_elmnt);
 	}, 10);
 
-	setTimeout(() => {
-		try {
-			// button.removeChild(ripple_elmnt);
-		} catch (er) {}
-	}, 400);
-	// button.addEventListener(
-	// 	"mouseup",
-	// 	(e) => {
-	// 		ripple_elmnt.style.opacity = 0;
-	// 		setTimeout(() => {
-	// 			try {
-	// 				button.removeChild(ripple_elmnt);
-	// 			} catch (er) {}
-	// 		}, 400);
-	// 	},
-	// 	{ once: true }
-	// );
-	// button.addEventListener(
-	// 	"blur",
-	// 	(e) => {
-	// 		ripple_elmnt.style.opacity = 0;
-	// 		setTimeout(() => {
-	// 			try {
-	// 				button.removeChild(ripple_elmnt);
-	// 			} catch (er) {}
-	// 		}, 450);
-	// 	},
-	// 	// { once: true }
-	// );
+	button.addEventListener(
+		"mouseup",
+		(e) => {
+			ripple_elmnt.style.opacity = 0;
+			setTimeout(() => {
+				try {
+					button.removeChild(ripple_elmnt);
+				} catch (er) {}
+			}, 500);
+		},
+		{ once: true }
+	);
+
+	button.addEventListener(
+		"mouseleave",
+		(e) => {
+			ripple_elmnt.style.opacity = 0;
+			setTimeout(() => {
+				try {
+					button.removeChild(ripple_elmnt);
+				} catch (er) {}
+			}, 500);
+		},
+		{ once: true }
+	);
+
+	button.addEventListener(
+		"blur",
+		(e) => {
+			ripple_elmnt.style.opacity = 0;
+			setTimeout(() => {
+				try {
+					button.removeChild(ripple_elmnt);
+				} catch (er) {}
+			}, 500);
+		},
+		{ once: true }
+	);
 }
